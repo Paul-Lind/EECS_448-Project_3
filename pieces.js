0f872b7983cad972a6cff9281fbfabf2605f721e
Konrad Kahnert <kkahnert49@gmail.com>
2021-10-28 14:32:10 -0500

    pieces can now be selected and moved to tiles (checking for legal moves not implemented)

--- pieces.js
+++ pieces.js
@@ -1,7 +1,11 @@
+let isPieceSelected = false;
+let selectedPiecePosition;
+
 let board = [];
 document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < 64; i++) {
-        board.push(i);
+        board.push('e');
+        document.getElementById(i).addEventListener("click", tileClicked);
     }
     placePieces();
 })
@@ -51,4 +55,47 @@ function placePieces() {
         }
     }
     console.log(board);
+    console.log(document);
+}
+
+/**
+ * @author Konrad Kahnert
+ * @return {void}
+ * @description to be written
+ * */
+function tileClicked()
+{
+  if (board[this.id] == 'r' || board[this.id] == 'b') // if player clicked piece
+  {
+    // select clicked piece
+    isPieceSelected = true;
+    selectedPiecePosition = this.id;
+
+    console.log("Selected Piece " + this.id);
+  }
+  else if (isPieceSelected == true) // if player clicked empty tile and a piece is selected
+  {
+    // move piece to empty tile
+    board[this.id] = board[selectedPiecePosition]
+
+    var selectedTileElement = document.getElementById(selectedPiecePosition);
+    selectedTileElement.removeChild(selectedTileElement.firstChild);
+
+    var pieceImage = document.createElement('img');
+
+    if (board[selectedPiecePosition] == 'r')
+    {
+      pieceImage.src = 'redPiece.png';
+    }
+    else if (board[selectedPiecePosition] == 'b')
+    {
+      pieceImage.src = 'blackPiece.png';
+    }
+
+    this.appendChild(pieceImage);
+
+    console.log("Placed " + board[selectedPiecePosition] + " piece at " + this.id);
+    board[selectedPiecePosition] = 'e';
+    isPieceSelected = false;
+  }
 }
